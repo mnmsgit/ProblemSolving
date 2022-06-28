0624bac9b6eaaceef4f886c3466101e47d77ec77
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-06-28 14:52:58 +0900

    [Silver IV] Title: 점화식, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/13699. 점화식/점화식.py
+++ 백준/Silver/13699. 점화식/점화식.py
@@ -1,23 +1,16 @@
-n = int(input())
-result_memo = [0 for _ in range(n+1)]
-result_memo[0] = 1
+import sys
+input = sys.stdin.readline
 
 
-def function_t(num):
-    result = 0
-    if num == 0:
+def t(n):
+    if not n:
         return 1
-    for k in range(num):
-        if result_memo[k] == 0 and result_memo[num - 1 - k] == 0:
-            result += function_t(k) * function_t(num - 1 - k)
-        elif result_memo[k] == 0:
-            result += function_t(k)*result_memo[num - 1 - k]
-        elif result_memo[num - 1 - k] == 0:
-            result += result_memo[k] * function_t(num - 1 - k)
-        else:
-            result += result_memo[k]*result_memo[num - 1 - k]
-    result_memo[num] = result
-    return result
+    if not dp[n]:
+        for i in range(n):
+            dp[n] += t(i)*t(n-1-i)
+    return dp[n]
 
 
-print(function_t(n))
+n = int(input())
+dp = [0]*36
+print(t(n))
