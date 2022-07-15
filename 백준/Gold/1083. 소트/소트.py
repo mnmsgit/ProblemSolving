fc34fd43429687676eb4420a95bbc3fa7ed9f1ff
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-07-15 16:02:00 +0900

    Update 소트.py

--- 백준/Gold/1083. 소트/소트.py
+++ 백준/Gold/1083. 소트/소트.py
@@ -1,25 +1,27 @@
 """
-s를 첫째 수를 가장 크게 하는데 먼저 사용
+사전상의 배열은 가장 앞의 숫자가 클수록 항상 더 크기 때문에 그리디 알고리즘을 이용해 풀 수 있는 sort 문제이다.
+오름차수 배열과 원래의 배열에서 정답인 부분을 pop하고 새로운 배열에 정답인 부분을 append 하여 구했으며 마지막에는 원래의 배열에 남은 값들을 extend하여 종료함.
+
 """
 import sys
 
 n = int(sys.stdin.readline())
 nums = list(map(int, input().split()))
 s = int(sys.stdin.readline())
-nums_sorted = sorted(nums, reverse=True)
 
 
 def s_sort(count, arr):
+    arr_sorted = sorted(arr, reverse=True)
     ans = []
-    i = 0
-    j = 0
-    k = len(arr)
+    i = 0  # i는 0 ~ k-j 사이의 값으로 범위 조건이 없어도 while조건 사이에서는 항상 존재하므로 index out range 에러가 생기지 않는다.
+    j = 0  # j는 sort가 완료된 갯수
+    k = len(arr)  # sort가 완료된 j는 총 배열의 개수인 k보다 작아야한다.
     while count and j < k:
-        index = arr.index(nums_sorted[i])
+        index = arr.index(arr_sorted[i])
         if index <= count:
-            ans.append(nums_sorted[i])
+            ans.append(arr_sorted[i])
             arr.pop(index)
-            nums_sorted.pop(i)
+            arr_sorted.pop(i)
             count -= index
             i = 0
             j += 1
@@ -30,3 +32,4 @@ def s_sort(count, arr):
 
 
 print(*s_sort(s, nums))
+
