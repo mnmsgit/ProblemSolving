6091ab874471e05eceaf899bfeae63c71f03ca9c
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-07-27 17:16:20 +0900

    Update 미로 탐색.py

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,3 +1,7 @@
+"""
+1.bfs를 사용한 이유: 전체 탐색 이면서 최단거리를 구하기 때문 2. 미로 배열을 미로보다 크게 잡은 이유: index error 를 방지하기 위함(0은 갈수없는 곳 이므로 벽도 0으로 처리)
+bfs로 구한 최단경로가 최단경로인 이유: 트리에서 최단경로가 아닌 경우는 이미 visited한 경우이기 때문에 제거됨
+"""
 import sys
 from collections import deque
 N, M = map(int, sys.stdin.readline().split())
@@ -7,7 +11,6 @@ graph = []
 for i in range(1, N+1):
     line = sys.stdin.readline().strip()
     maze[i] = list(map(int, list('0'+line+'0')))
-
 ans = 0
 
 
@@ -17,7 +20,7 @@ def bfs(x, y):
     queue = deque()
     queue.append([x, y, count])
     visited[x][y] = True
-    vectors = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    vectors = [[1, 0], [0, 1], [-1, 0], [0, -1]]   # x,y좌표가 나아갈 수 있는 방향을 저장한 배열
     while queue:
         node = queue.popleft()
         count = node[2]
