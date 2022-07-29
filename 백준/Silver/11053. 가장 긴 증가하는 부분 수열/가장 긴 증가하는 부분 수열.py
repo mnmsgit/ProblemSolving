89d1dfb86c7c3bb11ede24ce5b4b3827f28894d0
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-07-29 16:46:06 +0900

    Update 가장 긴 증가하는 부분 수열.py

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,3 +1,8 @@
+"""
+dp를 이용한 기본적 문제
+dp[n]항상 답일 것이라는 편견때문에 오래 걸린 문제
+또한 test case가  N (1 ≤ N ≤ 1,000) 이므로 n2의 시간복잡도도 가능함.
+"""
 n = int(input())
 a = list(map(int, input().split()))
 dp = [0 for i in range(n)]
