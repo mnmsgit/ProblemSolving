e041a465c0a61ef3e687eade5057a7057de0d985
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-12 17:44:09 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 216 ms, Memory: 33692 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -3,20 +3,16 @@ from heapq import *
 N = int(sys.stdin.readline())
 arr = []
 ans = 0
-part_sum = []
 for i in range(N):
     card = int(sys.stdin.readline())
-    arr.append(card)
-    ans += card
-heapify(arr)
-while len(arr) > 2:
-    a = heappop(arr)
-    b = heappop(arr)
-    part_sum.append(a+b)
-    heappush(arr, a+b)
-ans += sum(part_sum)
+    heappush(arr, card)
 if N == 1:
-    ans = 0
-print(ans)
-
-
+    print(0)
+else:
+    while len(arr) > 1:
+        a = heappop(arr)
+        b = heappop(arr)
+        partial_sum = a+b
+        ans += partial_sum
+        heappush(arr, partial_sum)
+    print(ans)
