c4067d55216055fb683bece945ace458a6fee23d
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-12 17:47:13 +0900

    Update 카드 정렬하기.py

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,3 +1,7 @@
+"""
+항상 작은 숫자 2개를 더하는 것이 전체로 봐도 가장 최적의 해인 그리디 알고리즘 문제
+작은 숫자 2개를 구하기 위해 우선순위 큐(heapq를 통해 구현)를 이용함
+"""
 import sys
 from heapq import *
 N = int(sys.stdin.readline())
@@ -16,3 +20,5 @@ else:
         ans += partial_sum
         heappush(arr, partial_sum)
     print(ans)
+
+
