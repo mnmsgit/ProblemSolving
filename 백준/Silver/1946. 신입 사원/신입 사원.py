46bf253d75ca6f9e8f7d26371114e3d0a2c06d8a
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-12 16:42:51 +0900

    [Silver I] Title: 신입 사원, Time: 3336 ms, Memory: 43336 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -4,17 +4,16 @@ T = int(sys.stdin.readline())
 for _ in range(T):
     N = int(sys.stdin.readline())
     A_sort = [[0]for _ in range(N+1)]
-    # B_sort = [[0]for _ in range(N+1)]
     for _ in range(N):
         a, b = map(int, sys.stdin.readline().split())
         A_sort[a] = b
-        # B_sort[b] = a
-    min_b = 0
+    max_b = A_sort[1]  # 지원자의 b등수가 max_b보다 낮아야 채용가능
     passer = 0
     for i in range(1, N+1):
-        if i == 1:
-            min_b = A_sort[i]
-        if A_sort[i] <= min_b:
+        b_rank = A_sort[i]
+        if b_rank <= max_b:
             passer += 1
-            min_b = A_sort[i]
+            max_b = b_rank
+            if b_rank == 1:
+                break
     print(passer)
