e2567963c45ecfb6bb8ad1b48fa49521e1461f78
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-12 15:07:40 +0900

    [Silver IV] Title: ATM, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,11 +2,11 @@ import sys
 N = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 arr.sort()
-ans = [0 for _ in range(N)]
+ans = []
 time = 0
 for i in range(N):
-    element = 0
-    for j in range(i+1):
-        element += arr[j]
-    ans[i] = element
+    if not i:
+        ans.append(arr[i])
+    else:
+        ans.append(ans[i-1]+arr[i])
 print(sum(ans))
