330e1f264f25f17e6ffa229883bc831e9032f0b2
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-17 15:11:23 +0900

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 32468 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+
+def bfs(arr, x, y):
+    arr[y][x] = 0
+    queue = deque()
+    queue.append((x, y))
+    vector_x = [1, 0, -1, 0]
+    vector_y = [0, 1, 0, -1]
+    while queue:
+        node = queue.popleft()
+        for v in range(4):
+            xn = node[0] + vector_x[v]
+            yn = node[1] + vector_y[v]
+            if graph[yn][xn]:
+                queue.append((xn, yn))
+                arr[yn][xn] = 0
+    return 1
+
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    M, N, K = map(int, sys.stdin.readline().split())
+    graph = [[0 for _ in range(M+2)] for _ in range(N+2)]
+    ans = 0
+    for _ in range(K):
+        X, Y = map(int, sys.stdin.readline().split())
+        graph[Y+1][X+1] = 1
+    for i in range(1, N+1):
+        for j in range(1, M+1):
+            if graph[i][j]:
+                ans += bfs(graph, j, i)
+    print(ans)
