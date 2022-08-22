2a4ecde9001738384371f82356a40b161714e0b9
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-22 17:26:57 +0900

    Update 체스판 다시 칠하기.py

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,8 +1,14 @@
+"""
+전체 탐색(브루트 포스)를 이용한 기본문제
+문제에서 4중 for문을 사용하지만 전체 경우의 수가 많지 않기 때문에 시간초과나지 않고 풀 수 있음
+체스판 배열의 (행, 열)에서 (홀,홀)과 (짝,짝)이 같아야 하기 때문에 합이 짝수인 경우로 풀이
+추가적으로 W,B로 시작하는 각각의 경우를 나눠서 계산하기 보다, 전체 경우(8*8)에서 하나의 경우를 빼고 둘중 작은 값을 기존 값과 비교해 구함
+"""
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
 board = [[]for _ in range(N)]
-ans = []
+ans = sys.maxsize
 for i in range(N):
     board[i] = list(sys.stdin.readline().strip())
 for i in range(N-7):
@@ -16,5 +22,5 @@ for i in range(N-7):
                 else:
                     if board[row][col] == 'B':
                         count += 1
-        ans.append(min(count, 64-count))
-print(min(ans))
+        ans = min(ans, min(count, 64-count))
+print(ans)
