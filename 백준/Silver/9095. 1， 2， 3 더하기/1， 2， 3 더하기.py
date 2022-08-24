d1855d853843b6de5711429bae55c924b850757c
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-24 17:05:06 +0900

    Update 1， 2， 3 더하기.py

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,7 +1,12 @@
+"""
+문제 자체는 쉬우나 dp로 풀어야한다는 강박 때문에 시간이 오래 걸린 문제
+테스트에서 마주칠 경우 알고리즘은 문제를 해결하기 위한 수단으로 사용해야한다. (알고리즘 쓰기 위해 푸는 것이 아니다.)
+순열로  풀고 팩토리얼 사용과정에서 dp사용
+"""
 import sys
 
 T = int(sys.stdin.readline())
-fac = [0for _ in range(11)]
+fac = [0 for _ in range(11)]
 for i in range(11):
     if i < 2:
         fac[i] = 1
@@ -23,3 +28,4 @@ for _ in range(T):
         three, two, one = element[0], element[1], element[2]
         ans += fac[one+two+three]//(fac[one]*fac[two]*fac[three])
     print(ans)
+
