dd7a0bfcc427134408fa25b2899efed6157ce012
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-08-25 17:47:04 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,31 +1,9 @@
-"""
-문제 자체는 쉬우나 dp로 풀어야한다는 강박 때문에 시간이 오래 걸린 문제
-테스트에서 마주칠 경우 알고리즘은 문제를 해결하기 위한 수단으로 사용해야한다. (알고리즘 쓰기 위해 푸는 것이 아니다.)
-순열로  풀고 팩토리얼 사용과정에서 dp사용
-"""
 import sys
 
 T = int(sys.stdin.readline())
-fac = [0 for _ in range(11)]
-for i in range(11):
-    if i < 2:
-        fac[i] = 1
-    else:
-        fac[i] = fac[i-1]* i
+dp = [0 for _ in range(11)]
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, 11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 for _ in range(T):
-    n = int(sys.stdin.readline())
-    case = []
-    ans = 0
-    rest = n
-    for i in range(n//3+1):
-        three = i
-        rest = n - (3*i)
-        for j in range(rest//2+1):
-            two = j
-            one = rest - (2*j)
-            case.append([three, two, one])
-    for element in case:
-        three, two, one = element[0], element[1], element[2]
-        ans += fac[one+two+three]//(fac[one]*fac[two]*fac[three])
-    print(ans)
-
+    print(dp[int(sys.stdin.readline())])
