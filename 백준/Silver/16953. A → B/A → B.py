8969231e22c85d5da546a9f2ff24f9433812e8e2
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-01-06 16:40:49 +0900

    Update A → B.py

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,3 +1,8 @@
+# top-down으로 생각하면 금방풀 수 있는문제
+# dp인줄 알고 풀었으나 메모리 초과로 불가함 -> 쉬운 방범생각
+# while 문 사용해도 되지만 무한 루프 문제로 최대 반복횟수를 미리 정해서품
+# 이 문제는 bfs로도  풀 수 있다. (queue에 현재원소 *2 , 현재원소 *10 +1) 넣는식 -> bottom-up
+
 import sys
 
 A,B = map(int, sys.stdin.readline().split())
