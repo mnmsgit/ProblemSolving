dc0f9ca57344b636fa514c6b15264faa0063cbfe
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-02-19 18:23:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int total,left,output = 0;
+	scanf("%d", &total);
+	left = total % 5;
+	if (left == 0) {
+		output = total / 5;
+	}
+	else if (left == 1) {
+		if (total < 6)
+		{
+			output = -1;
+		}
+		else
+		{
+			output = (total - 6) / 5 + 2;
+		}
+	}
+	else if (left == 2) {
+		if (total < 12) {
+			output = -1;
+		}
+		else {
+			output = (total - 12) / 5 + 4;
+		}
+	}
+	else if (left == 3) {
+		if (total < 3) {
+			output = -1;
+		}
+		else{
+			output = (total - 3) / 5 + 1;
+		}
+	}
+	else {
+		if (total < 9) {
+			output = -1;
+		}
+		else {
+			output = (total - 9) / 5 + 3;
+		}
+	}
+	
+	printf("%d", output);
+	return 0;
+	}
+	
