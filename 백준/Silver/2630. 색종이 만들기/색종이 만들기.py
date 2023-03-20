930fdea4cbdd3ebbc41b7eacfaccc1e05a9288f5
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-03-20 14:56:19 +0900

    Create 색종이 만들기.py

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,3 +1,6 @@
+# Divide and conquer 대표 문제
+# D&C는 일반적으로 재귀적으로 풀며 그렇기 때문에 top-down 방식이다.
+# 이 문제는  시간 줄이기 위한 목적이 아닌 D&C로 풀어야만 하는 문제 때문에
 import sys
 
 N = int(sys.stdin.readline())
@@ -10,17 +13,20 @@ ans = [0, 0]
 
 def search(start, size): # 시작점과 크기
     color = graph[start[0]][start[1]]
+    flag = True
     for i in range(size):
         for j in range(size):
             new_x = start[0] + i
             new_y = start[1] + j
             if graph[new_x][new_y] != color:
-                search(start, size//2)
-                search((start[0], start[1] + size//2), size//2)
-                search((start[0] + size//2, start[1]), size//2)
-                search((start[0] + size//2, start[1] + size//2), size//2)
-                return
-    ans[color] += 1
+                flag = False
+    if not flag:
+        search(start, size // 2)
+        search((start[0], start[1] + size // 2), size // 2)
+        search((start[0] + size // 2, start[1]), size // 2)
+        search((start[0] + size // 2, start[1] + size // 2), size // 2)
+    else:
+        ans[color] += 1
 
 
 search((0,0),N)
