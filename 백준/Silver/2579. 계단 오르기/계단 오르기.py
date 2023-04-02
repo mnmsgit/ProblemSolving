788b8440ad9dbc1d2b75d54e6dd0d162ffbb20f7
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-04-02 14:52:37 +0900

    Update 계단 오르기.py

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,3 +1,6 @@
+# dp 대표문제
+# 매번 전단계 계단을 밟을 지 안 밟을 지에 따라 경우가 2개이므로 배열을 2차원으로 구성
+
 import sys
 
 N = int(sys.stdin.readline())
@@ -8,7 +11,9 @@ stairs.insert(0,0)
 dp[1][0], dp[1][1] = stairs[1],stairs[1]
 
 for i in range(2,N+1):
+    # 전 계단을 안 밟는 경우
     dp[i][0] = max(dp[i-2]) + stairs[i]
+    # 전 계단을 밟는 경우
     dp[i][1] = dp[i-1][0] + stairs[i]
 
 print(max(dp[N]))
