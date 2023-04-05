f6154b9b6f809a38afa3319a366c4dde3b8f8314
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-04-05 23:08:40 +0900

    Update 레벨 햄버거.py

--- 백준/Silver/16974. 레벨 햄버거/레벨 햄버거.py
+++ 백준/Silver/16974. 레벨 햄버거/레벨 햄버거.py
@@ -1,3 +1,7 @@
+# dp 로 기본 베이스를 잡고 d_c로 풀어야하는 문제
+# b (전 단계 버거) p (전 단계 버거) b
+# 중간 p를 기준으로 n 과의 대소비교 3가지(같,작,크)
+# 문제 초기 조건은 항상 중요하니 꼭 체크하기(1시간 씀)
 import sys
 
 N, X = map(int, sys.stdin.readline().split())
@@ -15,7 +19,7 @@ for i in range(1,N):
 def d_c(n, level, ans):
     if not level:
         return 1
-    if n <=1:
+    if n <= 1:
         return 0
     mid = 2 + bnp[level-1]
     if mid > n:
