d31422b06c7c9605aa288b5f255fdebb1f07f3cb
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-04-10 00:13:37 +0900

    Update 행렬 곱셈 순서.py

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,3 +1,7 @@
+"""
+dp의 대표적 예시인 CMM문제
+자세한 내용은 첨부자료 참조
+"""
 import sys
 
 N = int(sys.stdin.readline())
@@ -8,11 +12,11 @@ for _ in range(N):
 dp = [[0 for _ in range(N+1)] for __ in range(N+1)]
 
 
-for col in range(1,N+1):
+for col in range(1, N+1):
     for row in range(col-1,0,-1):
-        candi = sys.maxsize
+        candi = sys.maxsize    # 최솟값 갱신을 위해 가장 큰 값을로 둠
         for k in range(row,col):
-            value = dp[row][k] + dp[k+1][col] + item[row][0] * item[k][1] * item[col][1]
+            value = dp[row][k] + dp[k+1][col] + item[row][0] * item[k][1] * item[col][1]  # 점화식 k는 앞 행렬과 뒷 행렬을 나누는 역할
             candi = min(candi,value)
         dp[row][col] = candi
 
