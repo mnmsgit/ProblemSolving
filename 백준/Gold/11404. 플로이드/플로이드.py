bf0ea1f26e17ea761f317059fdc0caafeeedd429
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-04-13 19:17:52 +0900

    [Gold IV] Title: 플로이드, Time: 240 ms, Memory: 116700 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,3 +1,7 @@
+"""
+플로이드-워셜 알고리즘을 그대로 구현해볼 수 있는 문제
+dp를 이용하며 3중 for 문이 필요한 문제
+"""
 import sys
 
 n = int(sys.stdin.readline())
@@ -10,9 +14,9 @@ for _ in range(m):
     a, b, c = map(int,sys.stdin.readline().split())
     cost[a][b] = min(cost[a][b],c)
 
-for k in range(1,n+1):
-    for first in range(1,n+1):
-        for des in range(1,n+1):
+for k in range(1, n+1):
+    for first in range(1, n+1):
+        for des in range(1, n+1):
             if first != des:
                 cost[first][des] = min(cost[first][des], cost[first][k]+cost[k][des])
 
