0104565908b6cbcd53bee35dff7fe5bdf2e64174
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-05-10 22:11:15 +0900

    Update 보물.py

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -9,3 +9,31 @@ ans = 0
 for i in range(N):
     ans += A[i]*B[i]
 print(ans)
+# import sys
+
+# N, M = map(int,sys.stdin.readline().split())
+
+# graph = [[] for _ in range(N)]
+
+# for i in range(N):
+#     graph[i] = list(map(int, sys.stdin.readline().split()))
+
+# for i in range(M):
+#     x1, y1, x2, y2 = map(int,sys.stdin.readline().split())
+#     row_iter = x2 -x1
+#     col_iter = y2-y1
+#     ans =0
+#     if col_iter:
+#         for j in range(x1-1, x2-1):
+#             if not row_iter:
+#                 for k in range(y2-1,y1-1):
+#                     ans += graph[j][k]
+#             else:
+#                 ans += graph[j][y2-1]
+#     else:
+#         if row_iter:
+#             for k in range(y2-1,y1-1):
+#                 ans += graph[x1-1][k]
+#         else:
+#             ans += graph[x1-1][y1-1]
+#     print(ans)
