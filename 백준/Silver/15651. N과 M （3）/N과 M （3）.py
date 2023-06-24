5740a152d8f22cfa432a17070fd1a84492cb0755
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-06-24 21:10:11 +0900

    [Silver III] Title: N과 M (3), Time: 1956 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,21 @@
+# backtracking 대표 문제 n,m(3)
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+num_list = [i for i in range(1, N+1)]
+visited = [False for i in range(N+1)]
+
+
+def backtracking(result):
+    if len(result) == M:
+        print(*result)
+        return
+    for num in num_list:
+        result.append(num)
+        # 재귀로 backtracking
+        backtracking(result)
+        # return 조건을 만족 못 한 경우
+        result.pop()
+
+
+backtracking([])
