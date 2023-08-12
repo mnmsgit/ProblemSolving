94a94184180d5f7be6af5e09f69935b4ba3d6506
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-08-12 15:05:21 +0900

    [Gold V] Title: 배, Time: 3288 ms, Memory: 115160 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,24 @@
+import sys
+
+N = int(sys.stdin.readline())
+limits = list(map(int, sys.stdin.readline().split()))
+limits.sort(reverse=True)
+M = int(sys.stdin.readline())
+boxs = list(map(int, sys.stdin.readline().split()))
+boxs.sort(reverse=True)
+
+if boxs[0] > limits[0]:
+    print(-1)
+    exit(0)
+
+time = 0
+while boxs:
+    if not boxs:
+        break
+    for limit in limits:
+        for box in boxs:
+            if limit >= box:
+                boxs.remove(box)
+                break
+    time += 1
+print(time)
