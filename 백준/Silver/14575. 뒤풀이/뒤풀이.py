e1ae0ae40db3ef7ab0dfa2177e589ce22987e275
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-08-21 16:25:48 +0900

    [Silver I] Title: 뒤풀이, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/14575. 뒤풀이/뒤풀이.py
@@ -0,0 +1,38 @@
+import sys
+
+N, T = map(int,sys.stdin.readline().split())
+L = []
+R = []
+range_set = []
+for i in range(N):
+    l, r = map(int, sys.stdin.readline().split())
+    L.append(l)
+    R.append(r)
+    range_set.append([l, r])
+
+if sum(L) > T or sum(R) < T:
+    print(-1)
+    exit(0)
+
+low = max(L)
+high = max(R)
+
+
+range_set.sort(key=lambda x : x[0])
+ans = high
+while low < high:
+    mid = (low + high) // 2
+    temp = 0
+    for element in range_set:
+        if mid > element[1]:
+            temp += element[1]
+        else:
+            temp += mid
+
+    if temp >= T:
+        ans = mid
+        high = mid
+    else:
+        low = mid+1
+
+print(ans)
