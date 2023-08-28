0329a96ce47fec4e3395edf120c1126639d5e369
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-08-28 17:42:51 +0900

    [Gold IV] Title: 스도쿠, Time: 744 ms, Memory: 120308 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+graph = [[]for i in range(9)]
+row = [[True, False, False, False, False, False, False, False, False, False] for _ in range(9)]
+col = [[True, False, False, False, False, False, False, False, False, False] for _ in range(9)]
+square = [[True, False, False, False, False, False, False, False, False, False] for _ in range(9)]
+
+
+def backtracking(idx):
+    global graph
+    global arr
+    if idx == len(arr):
+        for i in range(9):
+            print(*graph[i])
+        exit(0)
+        return
+    x, y = arr[idx]
+    for candidate in range(1,10):
+        if not row[x][candidate] and not col[y][candidate] and not square[(x//3)*3 + (y//3)][candidate]:
+            row[x][candidate] = True
+            col[y][candidate] = True
+            square[(x//3)*3 + (y//3)][candidate] = True
+            graph[x][y] = candidate
+            backtracking(idx+1)
+            graph[x][y] = 0
+            row[x][candidate] = False
+            col[y][candidate] = False
+            square[(x//3)*3 + (y//3)][candidate] = False
+
+
+arr = []
+for i in range(9):
+    line = list(map(int,input().split()))
+    graph[i] = line
+    for j in range(9):
+        value = line[j]
+        if value:
+            row[i][value] = True
+            col[j][value] = True
+            square[(i//3)*3 + (j//3)][value] = True
+        else:
+            arr.append((i,j))
+
+backtracking(0)
