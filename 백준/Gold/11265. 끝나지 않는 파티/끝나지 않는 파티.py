6813fb5d8fa035110f7a0ffb14bd4df88c0cde4f
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-09-05 17:49:33 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 468 ms, Memory: 116456 KB -BaekjoonHub

--- 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.py
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.py
@@ -1,13 +1,6 @@
 import sys
-INF = 10000000000
 N, M = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split()))for _ in range(N)]
-distance = [[INF for __ in range(N)]for _ in range(N)]
-
-
-def party(start, dist, time):
-    return time >= graph[start][dist]
-
 
 for k in range(N):
     for i in range(N):
@@ -17,7 +10,7 @@ for k in range(N):
 
 for i in range(M):
     A, B, C = map(int, sys.stdin.readline().split())
-    if party(A-1, B-1, C):
+    if C >= graph[A-1][B-1]:
         print("Enjoy other party")
     else:
         print("Stay here")
