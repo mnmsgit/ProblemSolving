22ee84d1ddac921aff254dfaf5396eb0a9013a44
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-09-15 13:48:32 +0900

    [Silver I] Title: 회의실 배정, Time: 308 ms, Memory: 44932 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,7 +1,3 @@
-"""
-그리디 알고리즘을 이용한 기초문제
-항상 끝나는 시간을 최소로 하는것이 최적의 해를 구하는 방법
-"""
 import sys
 
 N = int(sys.stdin.readline())
@@ -18,6 +14,6 @@ for i in range(N):
     elif meeting[i][0] >= last:
         last = meeting[i][1]
         count += 1
-
+    
 
 print(count)
