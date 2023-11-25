0e7909205072da8f40bfe9f45aece63b68f6f595
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2023-11-26 01:31:10 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 340 ms, Memory: 125236 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,42 +1,40 @@
 import sys
 
+V, E = map(int, sys.stdin.readline().split())
 
-def find(parent, x):
+
+def find(parent,x):
     if parent[x] == x:
         return x
-    parent[x] = find(parent, parent[x])
+    parent[x] = find(parent,parent[x])
     return parent[x]
 
 
-def union(parent, node1, node2):
-    root_a = find(parent, node1)
-    root_b = find(parent, node2)
+def union(parent,a,b):
+    rootA = find(parent,a)
+    rootB = find(parent,b)
 
-    if root_a < root_b:
-        parent[root_b] = root_a
+    if rootA < rootB:
+        parent[rootB] = rootA
     else:
-        parent[root_a] = root_b
+        parent[rootA] = rootB
 
 
-V, E = map(int,sys.stdin.readline().split())
-parent = [0] * (V+1)
+parent = [i for i in range(V+1)]
+
+
 edges = []
-result =0
 
-# 부모 테이블상에서, 부모를 자기 자신으로 초기화
-for i in range(1, V + 1):
-    parent[i] = i
+for _ in range(E):
+    a, b, cost = map(int, sys.stdin.readline().split())
+    edges.append((a, b, cost))
 
-for i in range(E):
-    a, b, c = map(int,sys.stdin.readline().split())
-    # 인접행렬 대신 연결 그래프로 표현 (value,목적지)
-    edges.append((c,a,b))
+edges.sort(key=lambda x: x[2])
 
-edges.sort()
-for edge in edges:
-    c, a, b = edge
-    # 사이클이 발생하지 않는 경우에만 집합에 포함(=연결한다.)
-    if find(parent, a) != find(parent, b):
-        union(parent, a, b)
-        result += c
-print(result)
+ans = 0
+for a,b,cost in edges:
+    if find(parent,a) != find(parent,b):
+        union(parent,a,b)
+        ans += cost
+
+print(ans)
