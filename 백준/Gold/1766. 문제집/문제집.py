dc7141585241d094eba1c5eac0225ef252ac3ce9
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2024-03-05 20:22:49 +0900

    [Gold II] Title: 문제집, Time: 204 ms, Memory: 40380 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -14,12 +14,12 @@ for i in range(M):
     indegree[des] += 1
     graph[start].append(des)
 
-# 들어오는 간선이 0인 경우 큐에 추가
+# 들어오는 간선이 0인 경우 우선순위 큐에 추가
 
 for i in range(1, N+1):
     if indegree[i] == 0:
-        heapq.heappush(p_queue,i)
-
+        heapq.heappush(p_queue, i)
+# 우선순위큐와 위상정렬을 이용하여 조건을 만족하는 사전순으로 가장 작은 값을 구함
 while p_queue:
     node = heapq.heappop(p_queue)
     ans.append(node)
