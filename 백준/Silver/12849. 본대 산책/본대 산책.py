35453b075c9ba8687a0373b9e1b5310808be61b4
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2024-06-28 15:26:52 +0900

    Update 본대 산책.py

--- 백준/Silver/12849. 본대 산책/본대 산책.py
+++ 백준/Silver/12849. 본대 산책/본대 산책.py
@@ -1,19 +1,23 @@
-D = int(input())
-dp = [0] * 8
-dp[0] = 1 
+import sys
 
-for i in range(D):
-    tmp = [0] * 8
-    tmp[0] = dp[1] + dp[2]
-    tmp[1] = dp[0] + dp[2] + dp[3]
-    tmp[2] = dp[0] + dp[1] + dp[3] + dp[4]
-    tmp[3] = dp[1] + dp[2] + dp[4] + dp[5]
-    tmp[4] = dp[2] + dp[3] + dp[5] + dp[6]
-    tmp[5] = dp[3] + dp[4] + dp[7]
-    tmp[6] = dp[4] + dp[7]
-    tmp[7] = dp[5] + dp[6]
+# Graph with manually implemented
 
-    for i in range(8):
-        dp[i] = tmp[i] % 1000000007
+adj_list = [[1, 7], [0, 2, 7], [1, 3, 6, 7], [2, 4, 6], [3, 5], [4, 6], [2, 3, 5, 7], [0, 1, 2, 6]]
 
-print(dp[0])
+graph = [[0 for _ in range(8)]for _ in range(8)]
+
+D = int(sys.stdin.readline())
+
+# table[n][m] : n -> node, m -> min
+table = [[0 for _ in range(D+1)]for _ in range(8)]
+table[0][0] = 1
+
+for m in range(1, D+1):
+    for n in range(8):
+        # n번째 node
+        n_node_sum = 0
+        for prev_node in adj_list[n]:
+            n_node_sum = (n_node_sum + table[prev_node][m-1]) % 1000000007
+        table[n][m] = n_node_sum
+
+print(table[0][D])
