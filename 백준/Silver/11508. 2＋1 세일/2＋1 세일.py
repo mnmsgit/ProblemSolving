f9f7859fd957a4f952588952de9e2a0da690e2eb
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2024-12-27 18:42:50 +0900

    [Silver IV] Title: 2+1 세일, Time: 112 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
@@ -1,3 +1,14 @@
+"""
+Greedy 기본문제
+
+local optima가 global optima인가?
+local optima: 하나의 그룹에서 최대한 많은 세일을 받는 것
+global optima: 전체적으로 많은 세일을 받는 것
+
+반례없음 -> Greedy 결정
+
+
+"""
 import sys
 
 N = int(sys.stdin.readline())
@@ -11,10 +22,10 @@ for i in range(N):
 item.sort(reverse=True)
 
 
-ans =0
+ans = 0
 
 for i in range(N):
-    if i % 3!=2:
+    if i % 3 != 2:
         ans += item[i]
 
 print(ans)
