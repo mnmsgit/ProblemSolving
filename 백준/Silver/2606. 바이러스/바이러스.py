1d99d3b61d2c663c87349035c6c8d0197a542297
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2025-04-10 01:12:20 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,38 +1,25 @@
-"""
-딕셔너리를 이용해 푸는 그래프 이론 문제
-큐를 이용해 문제의 조건의 맞는 경우를 빠지지 않고 찾는데 이용함
-
-+
-23/6/29 재체점 issue로 수정
-dictionary 사용 시 get 내장함수 사용 하여 key error exception 방지 ,bfs 풀이에서 dfs 풀이로 바꿈(둘 다 가능)
-"""
-import sys
-input = sys.stdin.readline
 
 N = int(input())
-pairs = int(input())
-graph = {}
-visited = [False for i in range(N+1)]
 
-for i in range(pairs):
-    a, b = map(int, input().split())
-    # key error를 피하기 위해 dictionary의 get 메서드 사용
-    graph[a] = graph.get(a, []) + [b]
-    graph[b] = graph.get(b, []) + [a]
+l = int(input())
 
-ans = 0
-visited[1] = True
-# stack for dfs
-stack = [1]
+graph =[[]for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
 
-while stack:
-    node = stack.pop()
-    neighbors = graph.get(node)
-    if neighbors is not None:
-        for neighbor in neighbors:
-            if not visited[neighbor]:
-                ans += 1
-                stack.append(neighbor)
-                visited[neighbor] = True
+def dfs(node):
+    global visited
+    visited[node] = True
+    for near in graph[node]:
+        if not visited[near]:
+            dfs(near)
+    return
 
-print(ans)
+for _ in range(l):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+dfs(1)
+
+print(visited.count(True)-1)
