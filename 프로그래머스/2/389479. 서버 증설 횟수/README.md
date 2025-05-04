85cdc34f0c957a2333609736407268d5c79ed576
HANSEONG LEE <102347351+mnmsgit@users.noreply.github.com>
2025-05-04 16:28:42 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.72 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.14 MB, 시간: 0.88 ms
+메모리: 9.12 MB, 시간: 0.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 20:17:31
+2025년 05월 04일 16:28:39
 
 ### 문제 설명
 
--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,30 +1,29 @@
 from collections import deque
 def solution(players, m, k):
+    debug_mode = False
+    
     answer = 0
-    # 19:50
-    # 그리디
+    # 그리디 알고리즘 접근
+    # 서버 큐 (start_time,end_time)
+    server_queue = deque()
     
-    server_count = 0
-    queue = deque()
+    # 턴마다 진행 로직
     
-    tick = 0
-    for player in players:
-        # 서버 반납
-        while queue:
-            if queue[0] <= tick:
-                queue.popleft()
-            else:
-                break
-        # 서버 증설
-        while True:
-            needed_server = player//m 
-            now_server_count = len(queue)
-            if now_server_count < needed_server:
-                queue.append(tick+k)
-                print(f"서버추가 턴 {tick}")
-                answer +=1
-            else:
-                break
-                
-        tick +=1
+    # 1. queue에서 가용시간 넘은 서버 제거(복수개 일 수 있음)
+    
+    # 2. 현재 서버로 진행 가능한지 확인
+    # 2.1 안되면 증설 즉, queue에 추가
+    
+    for turn, player in enumerate(players):
+        # 1
+        while server_queue and server_queue[0][1] <= turn:
+            server_queue.popleft()
+        # 2
+        while (len(server_queue)+1) * m <= player:
+            server_queue.append((turn,turn+k))
+            answer +=1
+            if debug_mode:
+                print(f"turn{turn}에서 서버 추가")
+    
+    
     return answer
